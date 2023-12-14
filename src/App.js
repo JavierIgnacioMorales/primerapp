667b285ce4a57318428ed6f1470ec9fbcdb9f57a
Javier Morales <javier.morales@educacion.gob.ar>
2023-12-14 06:32:41 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen los pobladores de Tesei:</h1>  
+        <Testimonio 
+          nombre='Emma Rodriguez'
+          pais='Argentina'
+          imagen='emma'
+          cargo='Fiambrera'
+          empresa='Quesitos del Oeste'
+          testimonio='Estoy muy contenta de cortar fiembre que se usara en todas las pizzas de <strong>Tesei</strong> y alrededores.'
+        />
+        <Testimonio 
+          nombre='Sarah Gomez'
+          pais='Argentina'
+          imagen='sarah'
+          cargo='Vendedora de Ropa'
+          empresa='Como me queda..!!'
+          testimonio='Esta lleno de gente con mal gusto y me siento muy orgullosa de poder ayudarlos en sus oufit.'
+        />
+        <Testimonio 
+          nombre='Chin Chu Lin'
+          pais='China'
+          imagen='shawn'
+          cargo='Dueño de Supermercado'
+          empresa='Supermercado Luna'
+          testimonio='這些人沒有把手，我不知道如何向員工支付工資，現在我整晚都拔掉冰箱的插頭。.'
+        />
+      </div>
     </div>
   );
 }
